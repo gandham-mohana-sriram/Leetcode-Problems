a1ff0f1a9416ceb009447648d56bd2ac555b6c70
gandham-mohana-sriram <gmsriram2007@gmail.com>
2026-09-22 21:24:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (54.40%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        p2,p1=1,2
+        for _ in range(3,n+1):
+            curr=p1+p2
+            p2=p1
+            p1=curr
+        
+        return p1
